74ca8bad6b33580208298723082cccdb6988a9b4
IfedayoEsho <ifedayo.esho@gmail.com>
2023-09-08 07:13:25 -0700

    A C program that prints Programming is like a multilingual language.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/* A program that prints programming is like building a multilingual puzzle. */
+#include <stdio.h>
+main()
+{
+	char Script[] = "Programming is like building a multilingual puzzle";
+
+	puts(Script);
+
+	return (0);
+}
